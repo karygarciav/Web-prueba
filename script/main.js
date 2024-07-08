165e85d35248be2368e29616c6d2b9f14da2b7cd
karygarciav <kary.garcia@code-studios.com>
2024-07-08 12:20:46 -0500

    Formulario en html y js

--- script/main.js
+++ script/main.js
@@ -1,21 +1,46 @@
-const nombre=document.getElementById("nombre");
-const email=document.getElementById("email");
-const password=document.getElementById("password");
 
 
 
 
-
-
-function captura(){
-
-    const nombre=document.getElementById("nombre").value;
-    if(nombre==="") {
-        alert('Por favor llenar campo nombre');
-        document.getElementById("nombre").focus;
-    }else{
-        console.log(nombre);
+function procesar(){
+    const formulario= document.forms['form1'];
+    const registro= {
+        "nombre": formulario.elements[0].value,
+        "activo": formulario.elements[1].value,
+        "email": formulario.elements[2].value,
+        "identificacion": formulario.elements[4].value,
+        "fechaNacimiento": formulario.elements[5].value,
     }
+
+   // registro.push(registro);
+    //formulario.reset();
+    console.log(formulario);
+    console.log(registro);
+
 
 
 }
+
+/*
+let hola=document.querySelector('#form1').value;
+
+console.log(hola);
+
+addEventListener('submit', e => {
+    e.preventDefault()
+    const data = Object.fromEntries(
+        new FormData(e.target.datoNombre)
+    )
+    console.log(data);
+});*/
+
+/*
+function captura() {
+    let data = array();
+
+    data=document.getElementById('form1').values;
+
+    console.log(data.'nombre');
+
+
+}*/
